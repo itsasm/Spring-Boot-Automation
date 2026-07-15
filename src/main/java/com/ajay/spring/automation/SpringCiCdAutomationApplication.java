d6ab3b927c9e6b25455c9e33e7b682f38553ef5d
AJAY-SINGH MEENA <meenaajay1992@gmail.com>
2026-07-15 23:59:11 +0530

    Initial Commit

---
+++ src/main/java/com/ajay/spring/automation/SpringCiCdAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.ajay.spring.automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCiCdAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCiCdAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajay/spring/automation/SpringCiCdAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajay.spring.automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCiCdAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
